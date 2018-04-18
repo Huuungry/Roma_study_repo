a45e8620c4248a4d124897943a451670fb59fc89
YuriJ Litvin <YuriJfake@gmail.com>
2018-04-18 06:53:21 +0300

    Added voise prononciation

--- sendbox.py
+++ sendbox.py
@@ -1,5 +1,10 @@
 import time
 import recognizing_code as aa
+from win32com.client import Dispatch
+
+speak = Dispatch("SAPI.SpVoice")
+speak.Speak("Hi there")
+
 
 print("Как тебя зовут?")
 aa.try_to_recognise()
